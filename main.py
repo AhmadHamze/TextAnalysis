35d4c2e6b9dfd03d5c6455301a5fa2f3b59fb574
Ahmad Hamze <ahmadhamze@yahoo.com>
2019-10-18 23:53:18 +0200

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import time
 sentence = "Once upon a time, there was a quite long sentence which served for testing reasons!"
 
 alphabet = [chr(i) for i in range(65,91)]
-alphabet_counter = [sentence.upper().count(chr(65+i)) for i in range(26)]
-print(alphabet_counter)
+start = time.perf_counter()
+
+#alphabet_counter = [sentence.upper().count(chr(65+i)) for i in range(26)] #THIS MIGHT BE FASTER!
+
+alphabet_counter = [0 for i in range(26)]
+for char in sentence.upper():
+    if char in alphabet:
+        alphabet_counter[alphabet.index(char)] += 1
+end = time.perf_counter()
+print(alphabet_counter,-start+end)
