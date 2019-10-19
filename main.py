d916f2c137ea0643e8c1ec4b5d7a3b5a4356af32
Ahmad Hamze <ahmadhamze@yahoo.com>
2019-10-20 01:40:49 +0200

    first text count

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import time
-sentence = "Once upon a time, there was a quite long sentence which served for testing reasons!"
 
 alphabet = [chr(i) for i in range(65,91)]
 start = time.perf_counter()
 
-#alphabet_counter = [sentence.upper().count(chr(65+i)) for i in range(26)] #THIS MIGHT BE FASTER!
+with open("small_text.txt",'r',encoding="utf-8") as file:
+    file_contents = file.read()
+    alphabet_counter = [file_contents.upper().count(chr(65+i)) for i in range(26)]
 
-alphabet_counter = [0 for i in range(26)]
-for char in sentence.upper():
-    if char in alphabet:
-        alphabet_counter[alphabet.index(char)] += 1
 end = time.perf_counter()
-print(alphabet_counter,-start+end)
+print(alphabet_counter,sum(alphabet_counter),-start+end)
+
+
+""" OLD VERSION
+with open("small_text.txt",'r',encoding="utf-8") as file:
+    file_contents = file.read()
+    for char in file_contents.upper():
+        if char in alphabet:
+            alphabet_counter[alphabet.index(char)] += 1
+"""
