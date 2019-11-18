68682622e68180db3e102c94000a59d46acbd6ea
Ahmad Hamze <ahmadhamze@yahoo.com>
2019-11-18 20:14:49 +0200

    bar plot

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-import time
+from matplotlib import pyplot as plt
 
 alphabet = [chr(i) for i in range(65,91)]
-start = time.perf_counter()
 
 with open("small_text.txt",'r',encoding="utf-8") as file:
     file_contents = file.read()
     alphabet_counter = [file_contents.upper().count(chr(65+i)) for i in range(26)]
 
-end = time.perf_counter()
-print(alphabet_counter,sum(alphabet_counter),-start+end)
-
-
-""" OLD VERSION
-with open("small_text.txt",'r',encoding="utf-8") as file:
-    file_contents = file.read()
-    for char in file_contents.upper():
-        if char in alphabet:
-            alphabet_counter[alphabet.index(char)] += 1
-"""
+plt.bar(alphabet,alphabet_counter,label="character repetition")
+plt.legend()
+plt.show()
