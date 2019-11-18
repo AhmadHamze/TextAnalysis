cbcbce327a4431185a6062652f732778f116e6e9
Ahmad Hamze <ahmadhamze@yahoo.com>
2019-11-18 20:28:53 +0200

    final touch 1

---
+++ README.md
@@ -0,0 +1,6 @@
+                                              TEXTANALYSIS
+
+This program counts the number of every character of the English alphabet in a text file ("small_text.txt").
+It will plot a bar chart showing the tendency of every character.
+
+The text provided in this example is a part of "The gift of the Magi".
--- main.py
+++ main.py
@@ -6,6 +6,6 @@ with open("small_text.txt",'r',encoding="utf-8") as file:
     file_contents = file.read()
     alphabet_counter = [file_contents.upper().count(chr(65+i)) for i in range(26)]
 
-plt.bar(alphabet,alphabet_counter,label="character repetition")
+plt.bar(alphabet,alphabet_counter,label="character tendency")
 plt.legend()
 plt.show()
